1e65809f2840ec77f048cee5951be4de0d37a2ad
Chang Kiat Foo <Chang-Kiat.Foo>
2024-10-18 11:10:57 +0800

    Clean project

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default function Document () {
+    return (
+        <Html>
+            <Head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="css/globals.css"/>
+            </Head>
+            <body>
+                <div className="container">
+                    <Main/>
+                </div>
+                <NextScript/>
+            </body>
+        </Html>
+    )
 }
